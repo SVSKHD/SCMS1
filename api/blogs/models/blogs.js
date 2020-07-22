4636a965bb00f1138392decc23f14b280657c7cb
hithesh <koushik.svsk@gmail.com>
2020-07-22 12:34:38 +0530

    webhook

--- api/blogs/models/blogs.js
+++ api/blogs/models/blogs.js
@@ -1,8 +1,30 @@
 'use strict';
-
+const axios=require('axios');
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+
+afterCreate:async entry=>{
+    axios.post(strapi.config.currentEnvironment.staticWebsiteBuildUrl, entry)
+    .catch(()=>{
+
+    });
+},
+afterUpdate:async entry=>{
+    axios.post(strapi.config.currentEnvironment.staticWebsiteBuildUrl, entry)
+    .catch(()=>{
+
+    });
+},
+afterDestroy:async entry=>{
+    axios.post(strapi.config.currentEnvironment.staticWebsiteBuildUrl, entry)
+    .catch(()=>{
+
+    });
+},
+
+
+};
