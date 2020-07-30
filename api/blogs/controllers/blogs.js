7291912d7b6cb9fd41dfe82a8be02804c78e3847
hithesh <koushik.svsk@gmail.com>
2020-07-30 16:14:53 +0530

    draft and categories initiation

--- api/blogs/controllers/blogs.js
+++ api/blogs/controllers/blogs.js
@@ -1,8 +1,16 @@
-'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  async find(ctx) {
+    let entities;
+    const filters=ctx.query;
+    filters.published=true;
+    if (ctx.query._q) {
+      entities = await strapi.services.blogs.search(filters);
+    } else {
+      entities = await strapi.services.blogs.find(filters);
+    }
 
-module.exports = {};
+    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.blogs }));
+  },
+};
